145d049bcb3229bca238202cb90577e8d178631b
Daniel Washington Ignacio <danielvertigo@hotmail.com>
2023-10-30 14:42:00 -0300

    Initial Commit

---
+++ PinchUI/PinchUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchUIApp.swift
+//  PinchUI
+//
+//  Created by Daniel Washington Ignacio on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
